d3ef81e76db0a60ac384e18613ff6923c15ac635
jlemosz <jonatan.lemos@unity3d.com>
2024-02-13 17:50:19 -0500

    Adding PreExport.cs

---
+++ Assets/Editor/PreExport.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+public class PreExport
+{
+    public static void PrintDefines()
+    {
+        string[] defines;
+        PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget), out defines);
+        Debug.Log("Custom defines from player settings: " + string.Join(";", defines));
+    }
+}
