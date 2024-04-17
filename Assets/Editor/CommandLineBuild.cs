acaf21bf0da382f0e38b26059c0801c54d248c9b
jlemosz <jonatan.lemos@unity3d.com>
2024-04-17 12:04:54 -0500

    Testing CommandLineBuild

--- Assets/Editor/CommandLineBuild.cs
+++ Assets/Editor/CommandLineBuild.cs
@@ -6,11 +6,12 @@ public static class CommandLineBuild
     {
         var target = UnityEditor.EditorUserBuildSettings.activeBuildTarget;
         var group = UnityEditor.BuildPipeline.GetBuildTargetGroup(target);
-            
+/*            
 #if UNITY_ANDROID
             Debug.Log("ANDROID GROUP: " + group.ToString());
 #elif UNITY_IOS
             Debug.Log("iOS GROUP: " + group.ToString());
 #endif
+*/
     }
 }
