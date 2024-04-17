653be501ea22301e8215afa4d6acda1df09f46bc
jlemosz <jonatan.lemos@unity3d.com>
2024-04-17 00:59:40 -0500

    Updating CommanLineBuild script

--- Assets/Editor/CommandLineBuild.cs
+++ Assets/Editor/CommandLineBuild.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 
-    public static class CommandLineBuild
+public static class CommandLineBuild
+{
+    public static void SetEnvironment()
     {
-        public static void SetEnvironment()
-        {
-            var target = UnityEditor.EditorUserBuildSettings.activeBuildTarget;
-            var group = UnityEditor.BuildPipeline.GetBuildTargetGroup(target);
+        var target = UnityEditor.EditorUserBuildSettings.activeBuildTarget;
+        var group = UnityEditor.BuildPipeline.GetBuildTargetGroup(target);
             
 #if UNITY_ANDROID
             Debug.Log("ANDROID GROUP: " + group.ToString());
 #elif UNITY_IOS
             Debug.Log("iOS GROUP: " + group.ToString());
 #endif
-        }
     }
+}
