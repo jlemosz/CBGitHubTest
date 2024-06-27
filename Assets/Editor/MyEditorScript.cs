83f128f166092a22456bc0415f7af64ab0484d0a
jlemosz <jonatan.lemos@unity3d.com>
2024-06-27 12:54:08 -0500

    Adding updates to MyEditorScript.

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -4,7 +4,7 @@ class MyEditorScript
    static void PerformBuild ()
    {
        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-       buildPlayerOptions.scenes = new[] {"Assets/Scenes/SampleScene.unity"};
+       buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
        buildPlayerOptions.options = BuildOptions.None;
        buildPlayerOptions.locationPathName = "Build";
